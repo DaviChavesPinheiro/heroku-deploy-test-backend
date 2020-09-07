214f2e0ede1f2e18d7a44f0b4031f970d474e30e
Davi <davicpinheiro123@hotmail.com>
2020-09-07 19:11:27 -0300

    Teste 1

---
+++ index.js
@@ -0,0 +1,18 @@
+const app = require('express')()
+const cors = require('cors')
+const bodyParser = require('body-parser')
+
+const port = process.env.PORT || 8080
+app.use(cors())
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json())
+
+
+app.use("/", (req, res) => {
+    res.status(200).send("TUDO OK")
+})
+app.use("/mangas", (req, res) => {
+    res.status(200).send("NARUTO E ONE PIECE")
+})
+
+app.listen(port, () => console.log(`Servidor Rodando na porta ${port} ` + new Date().toLocaleTimeString()))
