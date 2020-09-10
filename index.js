67253e18c62cf50b6e1d0950557749739d3cd752
Davi <davicpinheiro123@hotmail.com>
2020-09-10 16:20:53 -0300

    teste

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 const app = require('express')()
 const cors = require('cors')
 const bodyParser = require('body-parser')
-const admin = require("firebase-admin");
 
-const serviceAccount = require("./serviceAccountKey.json");
 
 const port = process.env.PORT || 8080
 app.use(cors())
@@ -11,26 +9,12 @@ app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json())
 
 app.use("/mangas", (req, res) => {
-    firestore.collection('mangas').listDocuments().then(snapShots => {
-        snapShots.forEach(document => {
-            document.get().then(value => {
-                res.status(200).send(value.data())
-            })
-        })
-    })
+    res.status(200).send("naruto")
 })
 
 app.use("/", (req, res) => {
     res.status(200).send("TUDO OK")
 })
-
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  databaseURL: "https://manga-reader-charlotte.firebaseio.com"
-});
-
-const firestore = admin.firestore();
-
 
 
 app.listen(port, () => console.log(`Servidor Rodando na porta ${port} ` + new Date().toLocaleTimeString()))
